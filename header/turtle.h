bf290ab3611deb65dc390384f8f175f32dbe646c
CM0use <dilanuzcs@gmail.com>
2022-04-16 09:51:40 -0400

    Update turtle.h

--- header/turtle.h
+++ header/turtle.h
@@ -1,7 +1,7 @@
 
 //          Copyright CM0use.
 // Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
+//    https://github.com/CM0use/Turtle-Graphics/blob/main/LICENSE
 //          https://www.boost.org/LICENSE_1_0.txt)
 
 # ifndef TURTLE_H
